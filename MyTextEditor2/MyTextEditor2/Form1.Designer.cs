35097efb195ae61790f3ac02fae82276143c0b33
Bryan Mendoza <bryanjmendoza@yahoo.com>
2015-01-09 00:29:18 -0600

    Visual Studio 2013 project

---
+++ MyTextEditor2/MyTextEditor2/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace MyTextEditor2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtEditorPane = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.enableToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtEditorPane
+            // 
+            this.txtEditorPane.AcceptsReturn = true;
+            this.txtEditorPane.AcceptsTab = true;
+            this.txtEditorPane.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtEditorPane.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEditorPane.Location = new System.Drawing.Point(12, 65);
+            this.txtEditorPane.Multiline = true;
+            this.txtEditorPane.Name = "txtEditorPane";
+            this.txtEditorPane.Size = new System.Drawing.Size(646, 364);
+            this.txtEditorPane.TabIndex = 0;
+            this.txtEditorPane.TextChanged += new System.EventHandler(this.txtEditorPane_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(670, 33);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.enableToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(50, 29);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(239, 30);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // enableToolStripMenuItem
+            // 
+            this.enableToolStripMenuItem.Name = "enableToolStripMenuItem";
+            this.enableToolStripMenuItem.Size = new System.Drawing.Size(239, 30);
+            this.enableToolStripMenuItem.Text = "Enable backup file...";
+            this.enableToolStripMenuItem.Click += new System.EventHandler(this.enableToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(239, 30);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(236, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(239, 30);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 30000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel3});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 411);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(670, 30);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.BackColor = System.Drawing.Color.Silver;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(181, 25);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+         
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.BackColor = System.Drawing.Color.Silver;
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(293, 25);
+            this.toolStripStatusLabel2.Spring = true;
+            this.toolStripStatusLabel2.Text = "toolStripStatusLabel2";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.BackColor = System.Drawing.Color.Silver;
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(181, 25);
+            this.toolStripStatusLabel3.Text = "toolStripStatusLabel3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumBlue;
+            this.ClientSize = new System.Drawing.Size(670, 441);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.txtEditorPane);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "MyEditor";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEditorPane;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.ToolStripMenuItem enableToolStripMenuItem;
+    }
+}
+
---
+++ MyTextEditor2/MyTextEditor2/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyTextEditor2
+{
+    public partial class Form1 : Form
+    {
+
+        private bool IsChanged;
+
+        private string CurrentFilename;
+
+        public Form1()
+        {
+            InitializeComponent();
+            IsChanged = false;
+            this.CurrentFilename = "";
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("WUT");
+            this.Close(); //close "this" window
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (this.IsChanged)
+            {
+                DialogResult dr;
+
+                dr = MessageBox.Show("Do you want to save the current changes?",
+                    "MyEditor", MessageBoxButtons.YesNo);
+
+                if (dr == DialogResult.Yes)
+                {
+                    this.saveToolStripMenuItem_Click(sender, e);
+                }
+            }
+           // MessageBox.Show("Closing");
+
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            DialogResult dr;
+            //
+            // has text changed?
+            //
+            if (!this.IsChanged)
+                return;
+
+            if (this.CurrentFilename == "")
+            {
+                this.saveFileDialog1.FileName = "mytextfile.txt";
+                this.saveFileDialog1.Filter = "*.txt|*.txt";
+                this.saveFileDialog1.InitialDirectory =
+                  System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                dr = this.saveFileDialog1.ShowDialog();
+                if (dr == DialogResult.OK)  // save file:
+                {
+                    System.IO.File.WriteAllText(this.saveFileDialog1.FileName, this.txtEditorPane.Text);
+
+                    this.CurrentFilename = this.saveFileDialog1.FileName;
+                    this.IsChanged = false;  // reset flag:
+                }
+            }
+            else
+            {
+                System.IO.File.WriteAllText(this.CurrentFilename, this.txtEditorPane.Text);
+                this.IsChanged = false;  // reset flag:
+            }
+      
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            if (this.IsChanged)
+            {
+
+                DialogResult dr2;
+                dr2 = MessageBox.Show("Do you want to save the current changes?",
+                    "MyEditor", MessageBoxButtons.YesNo);
+
+                if (dr2 == DialogResult.Yes)
+                {
+                    this.saveToolStripMenuItem_Click(sender, e);
+                }
+            }
+
+            this.openFileDialog1.CheckFileExists = true;
+            this.openFileDialog1.FileName = "";
+            this.openFileDialog1.Filter = "*.txt|*.txt";
+            this.openFileDialog1.InitialDirectory =
+                    System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            DialogResult dr = this.openFileDialog1.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                this.txtEditorPane.Text = System.IO.File.ReadAllText(this.openFileDialog1.FileName);
+                this.CurrentFilename = this.openFileDialog1.FileName;
+                IsChanged = false;
+            }
+        }
+
+        private void txtEditorPane_TextChanged(object sender, EventArgs e)
+        {
+            IsChanged = true;
+            this.toolStripStatusLabel2.Text = "Chars: " + this.txtEditorPane.Text.Length;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            if (IsChanged && this.CurrentFilename != "")
+            {
+                System.IO.File.WriteAllText(this.CurrentFilename, this.txtEditorPane.Text);
+                this.IsChanged = false;
+            }
+        }
+
+        private void enableToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.txtEditorPane.TextChanged += new System.EventHandler(Backup_TextChanged);
+        }
+
+        private void Backup_TextChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("backup......");
+            System.IO.File.WriteAllText("_backup.txt", this.txtEditorPane.Text);
+        }
+
+    }
+}
